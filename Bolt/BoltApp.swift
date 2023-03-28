c57b47de2ca5f5fbf3f3875bf7677e75aeb3e7be
Aayush9029 <aayushpokharel36@gmail.com>
2023-03-28 13:56:19 -0400

    Initial Commit

---
+++ Bolt/BoltApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoltApp.swift
+//  Bolt
+//
+//  Created by Aayush Pokharel on 2023-03-28.
+//
+
+import SwiftUI
+
+@main
+struct BoltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
